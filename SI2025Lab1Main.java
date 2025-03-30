97d4b2ee267a8b2766125cce6e54173cb7a6f33c
Marija Gjorgjieva <djordjievam@gmail.com>
2025-03-30 17:55:54 +0200

    Implemented Tasks count per category method

--- SI2025Lab1Main.java
+++ SI2025Lab1Main.java
@@ -96,10 +96,14 @@ class TaskManager {
     }
 
     // 7. Count tasks per category
-    public Map<String, Integer> countTasksPerCategory() {
-        // TODO: Implement counting logic
-        return new HashMap<>();
+   public Map<String, Integer> countTasksPerCategory() {
+        Map<String, Integer> categoryCount = new HashMap<>();
+        for (Task task : tasks) {
+            categoryCount.put(task.getCategory(), categoryCount.getOrDefault(task.getCategory(), 0) + 1);
+        }
+        return categoryCount;
     }
+}
 
     // 8. Mark a task as completed by name
     public void markTaskCompleted(String name) {
@@ -120,7 +124,8 @@ public class SI2025Lab1Main {
         manager.addTask("Buy groceries", Priority.LOW, "Personal");
 
         // MISSING: Calls to the new methods that will be implemented
+System.out.println("\nTask count per category:");
+        System.out.println(manager.countTasksPerCategory());
 
-        manager.printTasks();
     }
 }
